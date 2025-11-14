9c164e0dfd8d2890bb0c24ed3c31d361eb18fdbc
Mwai Yusuf <35960235+mwaiyusuf@users.noreply.github.com>
2025-11-14 21:07:06 +0300

    Add basic arithmetic operations in script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+let num1 = 10;
+let num2 = 5;
+let sum = num1 + num2; // sum will be 15
+console.log("Addition:", sum);
+
+// Subtraction
+let difference = num1 - num2; // difference will be 5
+console.log("Subtraction:", difference);
+
+// Multiplication
+let product = num1 * num2; // product will be 50
+console.log("Multiplication:", product);
+
+// Division
+let quotient = num1 / num2; // quotient will be 2
+console.log("Division:", quotient);
